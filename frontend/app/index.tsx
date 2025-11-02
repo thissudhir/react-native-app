60c261c3d2996c009aa7e791f91e2d0a1e489178
Abhishek Yadav <abhishekworkhere@gmail.com>
2025-11-03 00:02:12 +0530

    UPDATE(theme): update dummy theme color for nativewind

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -2,9 +2,9 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex bg-red-500 items-center justify-center flex-1">
-      <Text className="text-white">
-        The beautiful game is about to come. Stay tuned! Parakh
+    <View className="flex items-center justify-center flex-1">
+      <Text className="text-light-300 text-5xl font-bold">
+        The beautiful game is about to come. Stay tuned!
       </Text>
     </View>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "#D6C7FF",
+          200: "#A8B5DB",
+          300: "#9CA4AB",
+        },
+        dark: {
+          100: "#221F3D",
+          200: "#0F0D23",
+        },
+        accent: "#AB8BFF",
+      },
+    },
   },
   plugins: [],
 }
